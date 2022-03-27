8e3200b74e51ebae1673d57e75b528a2f9aa774f
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-03-27 20:42:29 -0300

    praticando um pouco de http

---
+++ metodos-http.js
@@ -0,0 +1,12 @@
+const inputNome = document.querySelector('#nome');
+const inputAtracoes = document.querySelector('#atracoes');
+const inputDescricao = document.querySelector('#descricao');
+const inputData = document.querySelector('#data');
+const inputLotacao = document.querySelector('#lotacao');
+
+const formCriarEvento = document.querySelector('form');
+
+formCriarEvento.onsubmit = async (e) => {
+    e.preventDefault();
+
+}
